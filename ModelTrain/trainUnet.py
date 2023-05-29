2945e4007837e2a098f25db2ed13c5f5852ae4dc
Samuel Yanes <syanes@us.es>
2023-05-29 13:27:48 +0200

    solved

--- ModelTrain/trainUnet.py
+++ ModelTrain/trainUnet.py
@@ -77,7 +77,6 @@ pbar_epoch = tqdm(total=N_epochs, desc="Epoch progress: ")
 pbar_batch = tqdm(total=len(dataloader) // args.batch_size, desc="Batch progress: ")
 
 for epoch in tqdm(range(N_epochs), desc="Epochs: "):
-for epoch in tqdm(range(N_epochs), desc="Epochs: "):
 
 	running_loss = []
 	pbar_epoch.update(1)

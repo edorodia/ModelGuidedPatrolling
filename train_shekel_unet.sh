9b2d58569434b6aab5134038cc00245d6b15da3f
Samuel Yanes <syanes@us.es>
2023-06-22 12:18:32 +0200

    added drl_unet script

---
+++ train_shekel_unet.sh
@@ -0,0 +1,7 @@
+
+# Script to train DRL
+
+# Usage: ./train_shekel.sh
+python Training/Train.py --benchmark shekel --w_reward_weight 10.0 --i_reward_weight 1.0 --model deepUnet
+python Training/Train.py --benchmark shekel --w_reward_weight 1.0 --i_reward_weight 10.0 --model deepUnet
+python Training/Train.py --benchmark shekel --w_reward_weight 10.0 --i_reward_weight 10.0 --model deepUnet

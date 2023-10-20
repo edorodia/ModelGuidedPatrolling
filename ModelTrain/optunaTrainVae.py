936fd9fc659803ba62522cbdbe9b60aecd088744
Samuel Yanes <syanes@us.es>
2023-10-20 16:55:41 +0200

    optuna updated

--- ModelTrain/optunaTrainVae.py
+++ ModelTrain/optunaTrainVae.py
@@ -207,7 +207,7 @@ def objective(trial):
 
 if __name__ == "__main__":
 	study = optuna.create_study(direction="minimize")
-	study.optimize(objective, n_trials=30, timeout=600)
+	study.optimize(objective, n_trials=30)
 
 	pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
 	complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])

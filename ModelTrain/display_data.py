3f7ab562352fe87d427479c5d49c3bcd017787ff
edorodia <edoardo.rodiani@studenti.unimi.it>
2024-07-21 11:49:35 +0200

    added dependencies

--- ModelTrain/display_data.py
+++ ModelTrain/display_data.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 loaded_data = np.load('ModelTrain\Data\gts_shekel_train.npy')
-print("Ground Truths saved -> length: " + str(loaded_data.shape[0]))
+print("Ground Truths saved -> length: " + str(loaded_data.shape))
 #print(loaded_data)
 
 loaded_data = np.load('ModelTrain\Data\\trajectories_shekel_train.npy')
-print("Trajectories saved -> length: " + str(loaded_data.shape[1]))
+print("Trajectories saved -> length: " + str(loaded_data.shape))
+print("Trajectories saved -> length: " + str(loaded_data.shape[2:]))
 #print(loaded_data)
 
 
